65c0dcc38230660c3724a266d84aad1f62b717c2
Eugene Draitsev <ddrrai@gmail.com>
2020-10-10 01:03:56 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components/macro'
+import elementPicker from 'element-picker'
+import { Box, Button, Container, TextField } from '@material-ui/core'
+
+const Wrapper = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+`
+const UrlForm = styled.form`
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+`
+const Site = styled.object`
+  margin-top: 20px;
+  width: 100%;
+  height: calc(100vh - 100px);
+  border: 1px solid black;
+`
 
 function App() {
+  const [url, setUrl] = useState('')
+  const [inputValue, setInputValue] = useState('test.html')
+
+  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setUrl(inputValue)
+    elementPicker.init({
+      onClick: (element: any) => {
+        console.log(element)
+      },
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <UrlForm onSubmit={onSubmit}>
+        <TextField
+          label="Url"
+          value={inputValue}
+          variant="outlined"
+          size="small"
+          fullWidth
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Box ml={2} />
+        <Button variant="contained" color="primary" type="submit">
+          Go
+        </Button>
+      </UrlForm>
+
+      {url && (
+        <Site data={url} type="text/html">
+          Alternative Content
+        </Site>
+      )}
+    </Wrapper>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme, StylesProvider, ThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider as StyledProvider } from 'styled-components/macro'
+
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+const theme = createMuiTheme()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <StylesProvider injectFirst>
+        <CssBaseline />
+        <StyledProvider theme={theme}>
+          <App />
+        </StyledProvider>
+      </StylesProvider>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
